d5fc214c5d9c5e3525b5c0b457b38e2851517d1f
Kingsten100 <isak9910@gmail.com>
2025-03-21 11:54:31 +0100

    Gjort möjligt att registrera användare med POST, kryptera lösenordet och möjlighet att logga in med POST förfrågan

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { errorHandler, notFound } from './middleware/error.middleware.js'
 import productRoutes from './routes/product.route.js'
 import messageRoutes from './routes/message.route.js'
+import userRoutes from './routes/user.route.js'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json())
 
 app.use('/api/product', productRoutes)
 app.use('/api/message', messageRoutes)
+app.use('/api/auth', userRoutes)
 
 
 
